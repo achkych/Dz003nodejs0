1bf55800f9b1ddb3bbbb9f3015efb257d3eddfa6
Marat <achkych@gmail.com>
2024-04-17 08:41:47 +0400

    Dz003nodejs01

---
+++ Dz003nodejs01.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const { changeInfo, readInfo, writeInfo } = require('./counterReadWrite');
+const fs = require('fs');
+const path = require('path');
+const filepath = path.join(__dirname, 'counters.json');
+
+app.get('/', (req, res) => {
+    changeInfo('/', 5);
+    res.send(`<h1>Main</h1><br><a href="/about">About</a> Просмотров: ${readInfo('/')}`);
+});
+
+app.get('/about', (req, res) => {
+    changeInfo('/about', 3);
+    res.send(`<h1>About</h1><br><a href="/">Main</a> Просмотров: ${readInfo('/about')}`);
+});
+
+app.listen(3000);
